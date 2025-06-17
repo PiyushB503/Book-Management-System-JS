b6e7324c1b55d0011ba48a43f45da8ecce08ff9f
Piyush Bhardwaj <piyush.bhardwaj1@sourcefuse.com>
2025-06-17 12:40:30 +0530

    remove dist and node modules

--- dist/models/manual.js
+++
@@ -1,6 +0,0 @@
-import { Book } from './books.js';
-export class ManualBook extends Book {
-    constructor(title, author, isbn, publication_date, genre) {
-        super(title, author, isbn, publication_date, genre, true);
-    }
-}
--- dist/utils/arrayUtils.js
+++
@@ -1,9 +0,0 @@
-export function filterItems(items, predicate) {
-    return items.filter(predicate);
-}
-export function sortItems(items, compareFn) {
-    return [...items].sort(compareFn);
-}
-export function findItem(items, predicate) {
-    return items.find(predicate);
-}
